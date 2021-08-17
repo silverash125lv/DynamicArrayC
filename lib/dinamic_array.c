cc6ec6023cb6a87fde17ab25d4132abe7d103d5b
silverash125lv <silverash125lv@gmail.com>
2021-08-16 23:11:55 -0300

    Create a dinamic array files

---
+++ lib/dinamic_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, C!!!");
+    return 0;
+}
+
+int foo(int arg)
+{
+    printf("Foo Bar");
+    return 8;
+}
